1db9960e29281ff357735c049ee97c8c95cbab50
Haroon <haroon.ali.1104@gmail.com>
2023-06-28 16:11:41 +0100

    add check for user func to login API route, add check password func to login API route

--- admin/pages/api/login.js
+++ admin/pages/api/login.js
@@ -1,8 +1,20 @@
 const validate = require("./validations/users");
+const pool = require("./db/db");
+const bcrypt = require('bcrypt');
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { error } = validate(req.body);
   if (error) return res.status(400).json({ message: error.message });
 
-  res.status(200).json({ name: 'John Doe' })
+  try {
+    const rs = await pool.query("SELECT * FROM users WHERE email = $1", [req.body.email]);
+    if (rs.rowCount <= 0) return res.status(400).json({ message: "Invalid email or password." });
+
+    const validPassword = await bcrypt.compare(req.body.password, rs.rows[0].password);
+    if (!validPassword) return res.status(400).send({ message: "Invalid email or password." });
+
+    res.send("ok");
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
 }
