1340b81d62e08dfd75d8672e9e130186c8eb43c6
Haroon <haroon.ali.1104@gmail.com>
2023-06-28 21:27:02 +0100

    mobile app - logout button func

--- tablet/components/NavBar.js
+++ tablet/components/NavBar.js
@@ -1,9 +1,23 @@
 import { View, Pressable, Text, StyleSheet } from "react-native";
+import { useNavigation } from "expo-router";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function NavBar() {
+    const navigation = useNavigation();
+
+    const logout = async () => {
+        try {
+            await AsyncStorage.removeItem('token')
+
+            navigation.navigate("index");
+        } catch (e) {
+            console.log(e.message);
+        }
+    }
+
     return (
         <View style={styles.container}>
-            <Pressable style={styles.logoutBtn}>
+            <Pressable style={styles.logoutBtn} onPress={logout}>
                 <Text style={styles.logoutBtnText}>Logout</Text>
             </Pressable>
         </View>
