fbffccdea4b0e5c638764ff3a05fdc84f2d31d9a
Haroon <haroon.ali.1104@gmail.com>
2023-06-28 13:48:52 +0100

    create register form with email input

--- tablet/app/index.js
+++ tablet/app/index.js
@@ -1,9 +1,17 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useState } from "react";
+import { View, Text, StyleSheet, TextInput } from "react-native";
 
 export default function Home() {
+    const [email, setEmail] = useState("");
+
     return (
         <View style={styles.container}>
-            <Text style={{ fontSize: 30 }}>Hello World</Text>
+            <View style={styles.formPanel}>
+                <View>
+                    <Text style={styles.label}>Email</Text>
+                    <TextInput style={styles.input} value={email} onChangeText={setEmail} />
+                </View>
+            </View>
         </View>
     );
 }
@@ -11,8 +19,24 @@ export default function Home() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'white',
+        backgroundColor: 'black',
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    formPanel: {
+        backgroundColor: 'white',
+        borderRadius: 10,
+        padding: 50
+    },
+    label: {
+        fontSize: 20
+    },
+    input: {
+        fontSize: 20,
+        borderWidth: 1,
+        width: 300,
+        height: 50,
+        padding: 10,
+        marginTop: 5
     }
 });
