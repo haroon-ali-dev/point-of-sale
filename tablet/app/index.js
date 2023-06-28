75f3a2620317b9c2636c501676dee530d89b64dc
Haroon <haroon.ali.1104@gmail.com>
2023-06-28 14:28:24 +0100

    add password field to mobile app login form

--- tablet/app/index.js
+++ tablet/app/index.js
@@ -3,13 +3,19 @@ import { View, Text, StyleSheet, TextInput } from "react-native";
 
 export default function Home() {
     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     return (
         <View style={styles.container}>
             <View style={styles.formPanel}>
+                <Text style={styles.heading}>Login</Text>
                 <View>
                     <Text style={styles.label}>Email</Text>
                     <TextInput style={styles.input} value={email} onChangeText={setEmail} />
+                </View>
+                <View>
+                    <Text style={styles.label}>Password</Text>
+                    <TextInput style={styles.input} value={password} onChangeText={setPassword} />
                 </View>
             </View>
         </View>
@@ -28,13 +34,18 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         padding: 50
     },
+    heading: {
+        fontSize: 30,
+        textAlign: 'center',
+        marginBottom: 20
+    },
     label: {
         fontSize: 20
     },
     input: {
         fontSize: 20,
         borderWidth: 1,
-        width: 300,
+        width: 400,
         height: 50,
         padding: 10,
         marginTop: 5
