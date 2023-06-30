7e62b96a344bf2d809a2acb6b88b030e594c447f
Haroon <haroon.ali.1104@gmail.com>
2023-06-30 22:51:50 +0100

    revert

--- admin/pages/products/index.js
+++ admin/pages/products/index.js
@@ -3,13 +3,19 @@ import Head from 'next/head'
 
 export default function Products() {
     return (
-        <AuthOnly>
+        <AuthOnly><ProductsContent /></AuthOnly>
+    );
+}
+
+export function ProductsContent() {
+    return (
+        <>
             <Head>
                 <title>Products - POS Admin</title>
             </Head>
             <main>
                 <h1>Products</h1>
             </main>
-        </AuthOnly>
+        </>
     );
 }
