2a62f54d161cf6bb7cdf60582a56d80c691ad18d
Haroon <haroon.ali.1104@gmail.com>
2023-06-30 17:38:29 +0100

    create login form for admin

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -1,4 +1,6 @@
 import Head from 'next/head'
+import { Card, Form, Button } from 'react-bootstrap';
+
 import styles from '@/styles/Home.module.css'
 
 export default function Register() {
@@ -8,7 +10,28 @@ export default function Register() {
         <title>Register - POS Admin</title>
       </Head>
       <main>
-        <h1>Hello World</h1>
+        <Card>
+          <Card.Body>
+            <Card.Title>Login</Card.Title>
+            <Form>
+              <Form.Group className="mb-3" controlId="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="text"
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                />
+              </Form.Group>
+
+              <Button variant="success" type="submit">Login</Button>
+            </Form>
+          </Card.Body>
+        </Card>
       </main>
     </>
   )
