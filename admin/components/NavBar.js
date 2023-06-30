853ae1f0ae6a07951a5ece872426ec377575d913
Haroon <haroon.ali.1104@gmail.com>
2023-06-30 17:15:09 +0100

    complete navbar for admin

--- admin/components/NavBar.js
+++ admin/components/NavBar.js
@@ -1,14 +1,16 @@
 import Link from 'next/link';
 import { Nav, Navbar, Container } from 'react-bootstrap';
+import { House } from 'react-bootstrap-icons';
 
 import styles from "./NavBar.module.css";
 
 export default function NavBar() {
     return (
-        <Navbar expand="lg">
-            <Container>
+        <Navbar className={styles.navbar} expand="lg" bg='dark' data-bs-theme="dark">
+            <Container className={styles.navBarContainer}>
+                <Navbar.Brand href="/" as={Link}><House size={30} /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Collapse id="basic-navbar-nav" className={styles.navBarCollapse}>
                     <Nav>
                         <Nav.Link href="/" as={Link}>Register</Nav.Link>
                         <Nav.Link href="/login" as={Link}>Login</Nav.Link>
