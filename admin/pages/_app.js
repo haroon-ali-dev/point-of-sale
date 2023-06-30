1bc5f12c0470eee3eb062ff3a97130dbdafede68
Haroon <haroon.ali.1104@gmail.com>
2023-06-30 13:14:04 +0100

    setup font

--- admin/pages/_app.js
+++ admin/pages/_app.js
@@ -1,5 +1,22 @@
+import Head from 'next/head'
+import { Ysabeau } from 'next/font/google'
+
 import '@/styles/globals.css'
 
+const ysabeau = Ysabeau({
+  subsets: ["latin"],
+  weight: ["100", "300", "600"]
+});
+
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={ysabeau.className}>
+      <Head>
+        <meta name="description" content="Admin panel for point of sale system." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
