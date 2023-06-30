8b5b7e92348270e4c5fc221a05dd9cb6d346e023
Haroon <haroon.ali.1104@gmail.com>
2023-06-30 17:55:39 +0100

    setup react-hook-form validation with yup for register form for admin

--- admin/pages/index.js
+++ admin/pages/index.js
@@ -6,7 +6,24 @@ import * as yup from "yup";
 
 import styles from '@/styles/Home.module.css'
 
+const schema = yup.object({
+  email: yup.string().max(256).email().required().label("Email"),
+  password: yup.string().min(3).max(15).required().label("Password"),
+}).required();
+
 export default function Register() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema)
+  });
+
+  const onSubmit = async (data) => {
+    console.log(data);
+  }
+
   return (
     <>
       <Head>
@@ -16,19 +33,29 @@ export default function Register() {
         <Card>
           <Card.Body>
             <Card.Title>Register</Card.Title>
-            <Form>
+            <Form onSubmit={handleSubmit(onSubmit)}>
               <Form.Group className="mb-3" controlId="email">
                 <Form.Label>Email</Form.Label>
                 <Form.Control
                   type="text"
+                  {...register("email")}
+                  isInvalid={errors.email?.message}
                 />
+                <Form.Control.Feedback type="invalid">
+                  {errors.email?.message}
+                </Form.Control.Feedback>
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="password">
                 <Form.Label>Password</Form.Label>
                 <Form.Control
                   type="password"
+                  {...register("password")}
+                  isInvalid={errors.password?.message}
                 />
+                <Form.Control.Feedback type="invalid">
+                  {errors.password?.message}
+                </Form.Control.Feedback>
               </Form.Group>
 
               <Button variant="success" type="submit">Register</Button>
