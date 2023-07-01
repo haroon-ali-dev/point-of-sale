ba350edfbba5faf3184e052029c5cbee096329d3
Haroon <haroon.ali.1104@gmail.com>
2023-07-01 22:43:37 +0100

    create product in API

--- admin/pages/api/products.js
+++ admin/pages/api/products.js
@@ -1,9 +1,28 @@
 const validate = require("./validations/products");
 const pool = require("./db/db");
+import { nanoid } from "nanoid";
 
 export default async function handler(req, res) {
-  const { error } = validate(req.body);
-  if (error) return res.status(400).json({ message: error.message });
+  if (req.method === 'POST') {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).json({ message: error.message });
 
-  res.send("ok");
+    try {
+      const p_Id = nanoid(10);
+
+      const dbRes = await pool.query("INSERT INTO products (u_id, p_id, name, price, quantity) VALUES($1, $2, $3, $4, $5) RETURNING *", [
+        req.body.uId,
+        p_Id,
+        req.body.name,
+        req.body.price,
+        req.body.quantity
+      ]);
+
+      res.json(dbRes.rows[0]);
+    } catch (error) {
+      res.status(500).json({ message: error.message })
+    }
+  } else {
+    res.send("Method not allowed.");
+  }
 }
