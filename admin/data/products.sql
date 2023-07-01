bd090c718b53acf19f76ea77b1d84cb3dfcc3c52
Haroon <haroon.ali.1104@gmail.com>
2023-07-01 21:25:03 +0100

    update SQL files

--- admin/data/products.sql
+++ admin/data/products.sql
@@ -1,6 +1,11 @@
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
+    u_id INT,
+    p_id VARCHAR(10) UNIQUE,
     name VARCHAR(50),
     price NUMERIC(4, 2),
     quantity INT
+    CONSTRAINT fk_user
+      FOREIGN KEY(u_id) 
+	  REFERENCES users(id)
 )
--- admin/data/users.sql
+++ admin/data/users.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    email VARCHAR(256),
+    email VARCHAR(256) UNIQUE,
     password VARCHAR (1024)
 )
