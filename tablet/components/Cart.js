74aa2311ce346e5350db1c5c5d3269410dd499dd
Haroon <haroon.ali.1104@gmail.com>
2023-07-04 17:17:20 +0100

    tablet app - add some styling to cart

--- tablet/components/Cart.js
+++ tablet/components/Cart.js
@@ -1,15 +1,56 @@
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Text, View, Pressable, StyleSheet } from "react-native";
+import { Feather } from '@expo/vector-icons';
 
 export default function Cart({ cart }) {
     return (
         <FlatList
             data={cart}
             renderItem={({ item }) => (
-                <View>
-                    <Text>{item.name}</Text>
+                <View style={styles.containerMain}>
+                    <View style={styles.containerName}>
+                        <Text style={styles.name}>{item.name} x{item.quantity}</Text>
+                    </View>
+                    <View style={styles.containerPrice}>
+                        <Text style={styles.price}>{item.price}</Text>
+                    </View>
+                    <View style={styles.containerBtn}>
+                        <Pressable>
+                            <Feather name="trash-2" size={30} color="black" />
+                        </Pressable>
+                    </View>
                 </View>
             )}
             keyExtractor={item => item.id}
         />
     );
 }
+
+const styles = StyleSheet.create({
+    containerMain: {
+        flexDirection: 'row',
+        backgroundColor: 'green',
+        marginHorizontal: 10,
+        marginTop: 10
+    },
+    containerName: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    name: {
+        fontSize: 18
+    },
+    containerPrice: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    price: {
+        fontSize: 18
+    },
+    containerBtn: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
