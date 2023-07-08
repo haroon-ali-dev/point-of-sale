c7cd0d648a3c7ac8ad54a7c2880ecb2915660c40
Haroon <haroon.ali.1104@gmail.com>
2023-07-08 15:22:54 +0100

    insert order into database within a transaction

--- admin/pages/api/orders/index.js
+++ admin/pages/api/orders/index.js
@@ -1,29 +1,36 @@
 const validate = require("../validations/orders");
 const pool = require("../db/db");
+const moment = require("moment");
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).json({ message: error.message });
+    if (req.method === 'POST') {
+        const { error } = validate(req.body);
+        if (error) return res.status(400).json({ message: error.message });
 
-    res.send("ok");
+        const date = moment().format("YYYY-MM-DD");
 
-    // try {
-    //   const p_Id = nanoid(10);
+        const client = await pool.connect();
 
-    //   const dbRes = await pool.query("INSERT INTO products (u_id, p_id, name, price, quantity) VALUES($1, $2, $3, $4, $5) RETURNING *", [
-    //     req.body.uId,
-    //     p_Id,
-    //     req.body.name,
-    //     req.body.price,
-    //     req.body.quantity
-    //   ]);
+        try {
+            await client.query("BEGIN");
 
-    //   res.json(dbRes.rows[0]);
-    // } catch (error) {
-    //   res.status(500).json({ message: error.message });
-    // }
-  } else {
-    res.status(500).json({ message: "Method not allowed."});
-  }
+            await client.query("INSERT INTO orders (u_id, date, cart, total) VALUES($1, $2, $3, $4)", [
+                req.body.uId,
+                date,
+                JSON.stringify(req.body.cart),
+                req.body.total
+            ]);
+
+            await client.query("COMMIT");
+
+            res.json({ message: "Order created." });
+        } catch (error) {
+            await client.query('ROLLBACK');
+            res.status(500).json({ message: error.message });
+        } finally {
+            client.release();
+        }
+    } else {
+        res.status(500).json({ message: "Method not allowed." });
+    }
 }
