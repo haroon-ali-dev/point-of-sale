5df7431a8688ee7f71adc14667458010a2f9ad5e
Haroon <haroon.ali.1104@gmail.com>
2023-07-11 21:23:16 +0100

    display order info on single order page

--- admin/pages/orders/[id]/[token].js
+++ admin/pages/orders/[id]/[token].js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
-import { Card, Form, Button, Spinner, Alert, InputGroup } from 'react-bootstrap';
+import { Card, Table } from 'react-bootstrap';
+import moment from 'moment';
 
 export async function getServerSideProps({ params }) {
     const req = await fetch(`http://localhost:3000/api/orders/${+params.id}`, {
@@ -20,7 +21,30 @@ export default function Order({ order }) {
             </Head>
 
             <main>
-                <h1>{order.id}</h1>
+                <h1 className='heading'>Order #{order.id}</h1>
+
+                <Card className='card-list'>
+                    <Card.Body>
+                        <Table striped bordered hover>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Date</th>
+                                    <th>Items</th>
+                                    <th>Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{order.id}</td>
+                                    <td>{moment(order["date"]).utcOffset("+0100").format("DD-MM-YYYY")}</td>
+                                    <td>{order.cart.length}</td>
+                                    <td>£{order["total"]}</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Card.Body>
+                </Card>
             </main>
         </>
     );
