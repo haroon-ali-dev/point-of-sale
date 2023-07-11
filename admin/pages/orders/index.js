6dd83c526331701fcee36df6791ecabd5f4e2fbe
Haroon <haroon.ali.1104@gmail.com>
2023-07-11 21:02:14 +0100

    admin - display orders on page

--- admin/pages/orders/index.js
+++ admin/pages/orders/index.js
@@ -2,8 +2,9 @@ import Head from 'next/head'
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import { Card, Spinner, Table, Stack } from 'react-bootstrap';
-import { PencilSquare, Trash } from 'react-bootstrap-icons';
+import { CardList } from 'react-bootstrap-icons';
 import jwt from "jwt-decode";
+import moment from 'moment';
 
 import AuthOnly from '@/components/AuthOnly';
 
@@ -14,6 +15,7 @@ export default function Orders() {
 }
 
 export function OrdersContent() {
+    const [tokenData, setTokenData] = useState(["", ""]);
     const [loading, setLoading] = useState(true);
     const [orders, setOrders] = useState([]);
 
@@ -30,9 +32,9 @@ export function OrdersContent() {
                 const data = await res.json();
 
                 if (res.status === 200) {
-                    console.log(data);
                     setLoading(false);
                     setOrders(data);
+                    setTokenData([token, uId]);
                 } else {
                     console.log(data);
                 }
@@ -53,6 +55,44 @@ export function OrdersContent() {
             <main>
                 <h1 className='heading'>Orders</h1>
 
+                <Card className='card-list'>
+                    <Card.Body>
+                        {loading && (
+                            <Spinner className="ms-auto me-auto" style={{ display: 'block' }} animation="border" role="status" size="sm">
+                                <span className="visually-hidden">Loading...</span>
+                            </Spinner>
+                        )}
+                        {(!loading && orders.length <= 0) && (
+                            <p style={{ textAlign: 'center', margin: '0px' }}>No orders.</p>
+                        )}
+                        {(!loading && orders.length > 0) && (
+                            <Table striped bordered hover>
+                                <thead>
+                                    <tr>
+                                        <th>Date</th>
+                                        <th>Items</th>
+                                        <th>Total</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {orders.map((order, i) => (
+                                        <tr key={i}>
+                                            <td>{moment(order["date"]).utcOffset("+0100").format("DD-MM-YYYY")}</td>
+                                            <td>{order.cart.length}</td>
+                                            <td>£{order["total"]}</td>
+                                            <td>
+                                                <Stack direction="horizontal" gap={3}>
+                                                    <Link className='table-btns' href={`orders/${order.id}/${tokenData[0]}`}><CardList /></Link>
+                                                </Stack>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </Table>
+                        )}
+                    </Card.Body>
+                </Card>
             </main>
         </>
     );
