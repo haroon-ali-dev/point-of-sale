1040a6a2a3d4ae2e2036e4579004053838838dbb
Haroon <haroon.ali.1104@gmail.com>
2023-07-17 22:41:45 +0100

    add SSL requirement to database setup

--- admin/pages/api/db/db.js
+++ admin/pages/api/db/db.js
@@ -5,7 +5,8 @@ const pool = new Pool({
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    ssl: process.env.NODE_ENV === "development" ? false : true
 });
 
 module.exports = pool;
