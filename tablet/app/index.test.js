f38b89dce6fac55f041ec21287f9e3f7d197bca9
Haroon <haroon.ali.1104@gmail.com>
2023-08-17 18:24:01 +0100

    remove testing setup

--- tablet/app/index.test.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react-native';
-import { LoginContent } from '.';
-
--- tablet/jest.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    preset: 'react-native',
-    setupFiles: ['./node_modules/@testing-library/jest-native/extend-expect'],
-};
